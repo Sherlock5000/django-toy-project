edd325dd38ff58112e8db10c9e1a6ba4179fa770
Anirban Das <anirban.das.16@mountblue.tech>
2021-06-16 19:25:56 +0530

    Implemented CRUD operations

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -17,3 +17,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+	def __str__(self):
+		return self.product.name
