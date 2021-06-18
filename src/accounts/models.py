610eb7dc7e3314bae03d97adda7621570c52806e
Anirban Das <anirban.das.16@mountblue.tech>
2021-06-18 12:18:36 +0530

    Added all functionalities

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,21 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
+
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    category = models.CharField(max_length=200, null=True)
+	CATEGORY = (
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
 
-    def __str__(self):
-        return self.name
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=200, null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
 
+	def __str__(self):
+		return self.name
 
 class Order(models.Model):
 	STATUS = (
@@ -30,6 +45,11 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note = models.CharField(max_length=1000, null=True)
 
 	def __str__(self):
 		return self.product.name
+
+
+
+	
--- src/library/settings.py
+++ src/library/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# MEDIA_URL = '/images/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
